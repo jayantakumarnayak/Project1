ff25b208d735c186e9cbfe535576f87ee2f695d5
jayantakumarnayak <jayantakumarnayak480@gmail.com>
2024-01-06 08:43:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./styles/App.scss";
+import "./styles/contact.scss";
+import "./styles/footer.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/mediaquery.scss";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import React from "react";
+import Services from "./components/Services";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>Prarambh</h1>
+            <p>@all right reserved</p>   
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://www.youtube.com/@jayantakumarnayak6344" target={"blank"}>Youtube</a>
+                <a href="https://www.instagram.com/jaykumar6891" target={"blank"}>Instagram</a>
+                <a href="https://github.com/jayantakumarnayak" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
