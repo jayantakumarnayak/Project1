a3182806f3cdf4408e06f5d01063e4ee7fa70c38
jayantakumarnayak <jayantakumarnayak480@gmail.com>
2024-03-19 21:48:14 +0530

    minor fix
    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import "./styles/contact.scss";
 import "./styles/footer.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
-import "./styles/mediaquery.scss";
-import Brand from "./components/brand";
-import Contact from "./components/Contact";
+import "./styles/mediaquery.scss";import Brands from "./components/Brands";
+import Contacts from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -20,9 +19,9 @@ const App = () => {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contacts />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/brands" element={<Brand/>} />
+        <Route path="/brands" element={<Brands />} />
       </Routes>
       <Footer/>
     </Router>
---
+++ src/components/Brands.jsx
@@ -0,0 +1,28 @@
+import br1 from "../assets/br1.jpg";
+import br2 from "../assets/br2.png";
+import br3 from "../assets/br3.png";
+
+const Brands = () => {
+  return (
+    <div className="contact">
+      <div class="container">
+        <h1>Tech Brands Supporting Our Product</h1>
+        <br />
+        <br />
+        <br />
+        <ul class="brand-list">
+          <li>
+            <img src={br1} alt="item1" className="brand-logo" />
+          </li>
+          <li>
+            <img src={br3} alt="item1" className="brand-logo" />
+          </li>
+          <li>
+            <img src={br2} alt="item1" className="brand-logo" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Brands;
